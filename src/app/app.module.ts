a2a643b3987f76d924fa74fe3cd73284dbf0b846
Francisco <fdurante@alu.ucam.edu>
2022-03-11 12:57:11 +0100

    Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { contadorModule } from './contador/contador.module';
+
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
